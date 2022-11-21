2cdacdbe487f020db2fcaca887a88fca2577bdae
mmnisi021 <mmnisi021@student.wethinkcode.co.za>
2022-11-21 13:03:14 +0200

    kyahlangana

--- src/components/Why_Sovtech.js
+++ src/components/Why_Sovtech.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import Typical from "react-typical";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
 
 class Why_SovTech extends Component {
     render() {
@@ -14,8 +19,29 @@ class Why_SovTech extends Component {
           ]}
           />
           </div>
-          <div ></div>
-        </div>
+          
+          <div className="sovCard">
+          <Card 
+            style={{
+            
+            backgroundImage: "white",
+      }}
+      >
+      <CardContent>
+      <Typography>
+        My name is mbali
+      </Typography>
+
+      </CardContent>
+      
+      
+
+   </Card>
+      
+     
+          </div>
+          </div>
+       
       );
     }
   }
